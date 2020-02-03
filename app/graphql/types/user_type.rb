a0936af937cd3126f6d62b380b238ab38d1be6df
Ricardo Hurtado <ricardo.hurtado@trineo.com>
2020-02-04 10:56:41 +1300

    Add GQL type: user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,13 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :email, String, null: false
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
+
+    def posts_count
+      object.posts.size
+    end
   end
 end
