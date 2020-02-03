8b8411a27dfa88960bf1cc94a465d39d9b07d91b
Ricardo Hurtado <ricardo.hurtado@trineo.com>
2020-02-04 10:19:03 +1300

    Setup graphiql UI

--- app/graphql/api_schema.rb
+++ app/graphql/api_schema.rb
@@ -3,7 +3,9 @@ class ApiSchema < GraphQL::Schema
   query(Types::QueryType)
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
+  # Can't use `GraphQL::Execution::Interpreter` without `GraphQL::Analysis::AST`
   use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
