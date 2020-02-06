8fea8cdca221967f29e4ead24b95decfbc3489b5
Ricardo Hurtado <ricardo.hurtado@trineo.com>
2020-02-07 09:51:41 +1300

    Add query type for post

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, Integer, null: false
     field :title, String, null: false
     field :body, String, null: false
+    field :user, Types::UserType, null: false
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,27 @@ module Types
     def user(id:)
       User.find(id)
     end
+
+    field :posts, [Types::PostType], null: false do
+      argument :limit, Integer, required: false
+    end
+
+    def posts(limit: 5)
+      Post.all.limit(limit)
+    end
+
+    field :post, Types::PostType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def post(id:)
+      Post.find(id)
+    end
+
+    field :posts_count, Integer, null: false
+
+    def posts_count
+      Post.count
+    end
   end
 end
